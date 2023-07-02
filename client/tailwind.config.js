e6403e0c7975736e1c69b6d7d0a43b2c571d86ff
seyistry <seyistry@gmail.com>
2023-07-02 09:05:49 +0100

    add theme

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,6 +2,13 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      brand: {
+        100: '#FAFCFC',
+        200: '#EEF6FF',
+        500: '#3FAA6F',
+      }
+    },
     extend: {},
   },
   plugins: [],
