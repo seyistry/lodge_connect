9606bdd2f04aa70a777e5e4d7b2767121a968895
AfiaAniebiet <aniebietafia87@gmail.com>
2023-07-02 14:15:19 +0100

    added errorHandler middleware

--- server/src/app.js
+++ server/src/app.js
@@ -2,11 +2,20 @@
 import express from 'express';
 import cors from 'cors';
 
+import errorHandlerMiddleware from './middleware/errorHandler';
+import notFound from './middleware/notFound';
+
 const app = express();
 
-// setting up the modules object
+//middlewares
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// using the router
+
+// implementing the error middlewares
+app.use(errorHandlerMiddleware);
+app.use(notFound);
 
 export default app;
