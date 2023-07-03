e73d7e20662bbc47e7d2386bbdea1466bb5466c5
ridhwan <ridmartrite@gmail.com>
2023-07-03 17:17:42 +0100

    further solving merge error

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -95,7 +95,7 @@ const Register = () => {
             <div className="container mx-auto">
                 <div className='bg-white rounded-xl shadow-lg flex flex-col lg:flex-row w-10/12 lg:w-8/12 mx-auto overflow:hidden'>
                     {/* style the left image */}
-                    <div class="w-full lg:w-1/2 flex flex-col items-center justify-center p-12 bg-no-repeat bg-cover bg-center bg-reg-back" >
+                    <div class="w-full lg:w-1/2 flex flex-col items-center justify-center p-12 bg-no-repeat bg-cover bg-center bg-regback" >
                         <h1 class="text-white text-3xl mb-3">Welcome</h1>
                         <div>
                             <p class="text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean suspendisse aliquam varius rutrum purus maecenas ac <a href="#" class="text-purple-500 font-semibold">Learn more</a></p>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,19 +4,20 @@ export default {
   theme: {
 
     extend: {
+      colors: {
+        brand: {
+          100: '#FAFCFC',
+          200: '#EEF6FF',
+          500: '#3FAA6F',
+        }
+      },
       backgroundImage: {
-        'reg-back': "url('./src/assets/Register-Background.png')",
+        regback: "url('./src/assets/Register-Background.png')",
         
       }
     },
 
-    colors: {
-      brand: {
-        100: '#FAFCFC',
-        200: '#EEF6FF',
-        500: '#3FAA6F',
-      }
-    },
+   
 
   },
   plugins: [],
