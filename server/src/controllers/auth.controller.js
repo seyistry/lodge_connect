5fc09a1fe2d5d9112a616fa5248dd0fb1ae8ccea
AfiaAniebiet <aniebietafia87@gmail.com>
2023-07-05 18:40:25 +0100

    creating token for signup and login

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -28,32 +28,47 @@ export const registerUser = tryCatchHelper(async (req, res) => {
     expiresIn: process.env.DURATION,
   });
 
-  res.status(StatusCodes.CREATED).json({ user, token });
-  return successResponse(res, 'User created successfully', { user: tokenUser }, StatusCodes.CREATED);
+  return successResponse(
+    res,
+    'User created successfully',
+    { user: { fullName: `${user.first_name} ${user.last_name}`, email: user.email }, token },
+    StatusCodes.CREATED
+  );
 });
 
 export const userLogin = tryCatchHelper(async (req, res) => {
   const { email, password } = req.body;
+
+  // check for email and password
+  if (!email || !password) {
+    throw new AppError('Email or Password field cannot be empty', StatusCodes.BAD_REQUEST);
+  }
 
   //   Find user by email
   const user = await User.findOne({ email });
 
   // check if email exists in the database
   if (!user) {
-    throw new AppError('Invalid Email or Password. Please try again.', StatusCodes.BAD_REQUEST);
+    throw new AppError('Invalid Email or Password. Please try again.', StatusCodes.UNAUTHORIZED);
   }
 
   // retrieve password from the database and compare with entered password
   const validatePassword = await user.comparePassword(password);
   if (!validatePassword) {
-    throw new AppError('Invalid Email or Password. Please try again.', StatusCodes.BAD_REQUEST);
+    throw new AppError('Invalid Email or Password. Please try again.', StatusCodes.UNAUTHORIZED);
   }
 
-  //   create token to validate the user
-  const tokenUser = { email: user.email, userId: user._id };
-  addCookiesToResponse({ res, user: tokenUser });
+  //   create token to validate the user if the user exists
+  const token = jwt.sign({ userId: user._id, email: user.email }, process.env.JWT_SECRET, {
+    expiresIn: process.env.DURATION,
+  });
 
-  return successResponse(res, 'Login successful', {});
+  return successResponse(
+    res,
+    'Login successful',
+    { user: { fullName: `${user.first_name} ${user.last_name}`, email: user.email }, token },
+    StatusCodes.OK
+  );
 });
 
 export const logoutUser = (req, res) => {
