8986483d2000eda18a8cbaf90fa42b81a2066ad7
AfiaAniebiet <aniebietafia87@gmail.com>
2023-07-08 15:16:08 +0100

    added search query to fetch all apartments

--- server/src/routes/apartment.route.js
+++ server/src/routes/apartment.route.js
@@ -1,6 +1,11 @@
 import express from 'express';
 const router = express.Router();
 
+// import cloudinary and configure multer
+import multer from 'multer';
+import { storage } from '../config/cloudinary.js';
+const upload = multer({ storage });
+
 import { userAuth } from '../middlewares/authorization/user.auth.js';
 
 import {
@@ -14,9 +19,9 @@ import {
 
 router.get('/apartments', getAllApartments);
 router.get('/my-apartments', userAuth, ownerDashboard);
-router.post('/apartment', userAuth, postApartment);
+router.post('/apartment', userAuth, /*upload.array('image'),*/ postApartment);
 router.get('/apartment/:apartmentId', getSingleApartment);
-router.patch('/apartment/:apartmentId', userAuth, updateApartment);
+router.patch('/apartment/:apartmentId', /*userAuth, upload.array('image'),*/ updateApartment);
 router.delete('/apartment/:apartmentId', userAuth, removeApartment);
 
 export default router;
--- server/src/utils/helpers/utilFunctions.helper.js
+++ server/src/utils/helpers/utilFunctions.helper.js
@@ -5,3 +5,10 @@ export const checkPermissions = (requestUser, requestUserId) => {
   if (requestUser === requestUserId.toString()) return;
   throw new AppError("You're not authorized to modify this resource.", StatusCodes.UNAUTHORIZED);
 };
+
+export const pagination = (skip, limit) => {
+  const page = Number(req.query.page) || 1;
+  limit = Number(req.query.limit) || 5;
+  skip = (page - 1) * limit;
+  return skip;
+};
