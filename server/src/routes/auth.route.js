656b89ea8b0d3cee15b28005041335a267887676
Abiodun Shittu <jerrywizklay1@gmail.com>
2023-07-10 13:19:53 -0400

    Fix some bugs in User endpoints

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -16,21 +16,25 @@ import {
 	changePassword,
 } from "../controllers/auth.controller.js";
 import {
+	emailValidator,
 	loginValidator,
 	registerValidator,
+	otpValidator,
+	resetPasswordValidator,
+	changePasswordValidator,
 } from "../middlewares/validation/user.validation.js";
 import { userAuth } from "../middlewares/authorization/user.auth.js";
 
 router.post("/register", registerValidator, registerUser);
-router.post("/verify-email", verifyEmail);
-router.post("resend-otp", resendOtp);
-router.post("/forgot-password", forgotPassword);
-router.post("/verify-password-otp", verifyResetPassword);
-router.post("reset-password", resetPassword);
+router.post("/verify-email", otpValidator, verifyEmail);
+router.post("/resend-otp", emailValidator, resendOtp);
+router.post("/forgot-password", emailValidator, forgotPassword);
+router.post("/verify-password-otp", otpValidator, verifyResetPassword);
+router.post("/reset-password", userAuth, resetPasswordValidator, resetPassword);
 router.post("/login", loginValidator, userLogin);
 router.get("/profile", userAuth, getUserProfile);
 router.put("/profile", userAuth, updateUserProfile);
-router.post("change-password", changePassword);
+router.put("/change-password", userAuth, changePasswordValidator, changePassword);
 router.delete("/profile", userAuth, deleteUserProfile);
 
 export default router;
