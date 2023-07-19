e12616554c5dab87cb51a39d986c80b1e04550df
Oluwaseyi Egunjobi <seyistry@gmail.com>
2023-07-19 20:40:32 +0100

    increase input width

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -27,7 +27,7 @@ export default function Hero() {
             type="text"
             name="searchValue"
             id=""
-            className="transparent outline-none focus:ring-0  h-10 py-4 pl-4 w-[70%]"
+            className="transparent outline-none focus:ring-0  h-10 py-4 pl-4 w-[78%]"
             placeholder="Search Apartment..."
             // onChange={(event) => setSearchValue(event.target.value)}
             required
